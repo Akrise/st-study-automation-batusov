27d4fbfc4006c56749c1aeecc1958b3346e1d20d
Akrise <49941855+Akrise@users.noreply.github.com>
2021-11-14 18:02:04 +0300

    Тест-кейсы LoginAsActiveUserTest обновлены для интеграции с Allure

--- src/test/java/at/tests/selenium_tests/LoginAsActiveUserTest.java
+++ src/test/java/at/tests/selenium_tests/LoginAsActiveUserTest.java
@@ -1,44 +1,55 @@
 package at.tests.selenium_tests;
 
+import at.study.redmine.allure.asserts.AllureAssert;
 import at.study.redmine.api.ui.BrowserUtils;
 import at.study.redmine.model.User;
+import io.qameta.allure.Allure;
+import io.qameta.allure.Step;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class LoginAsActiveUserTest extends BaseUITest{
+public class LoginAsActiveUserTest extends BaseUITest {
 
     private User user;
 
-    @BeforeMethod
-    public void prepareFixtures(){
+    @BeforeMethod(description = "1. Заведен пользователь в системе.\n" +
+            "2. Пользователь подтвержден администратором и не заблокирован\n" +
+            "3. Браузер открыт на домашней странице")
+    public void prepareFixtures() {
         user = new User().create();
         openBrowser();
     }
 
-    @Test
-    public void loginAsActiveUserTest(){
+    @Test(description = "Авторизация подтвержденным пользователем")
+    public void loginAsActiveUserTest() {
+        Allure.step("Нажать на кнопку \"Вход\"");
         headerPage.login.click();
         loginPage.login(user);
         //1. Отображается домашняя страница
-        Assert.assertEquals(browser.getDriver().getCurrentUrl(), "http://edu-at.dfu.i-teco.ru/");
+        AllureAssert.assertEquals(
+                browser.getDriver().getCurrentUrl(),
+                "http://edu-at.dfu.i-teco.ru/",
+                "Отображается домашняя страница");
         //2. Отображается "Вошли как <логин пользователя>"
-        Assert.assertEquals(headerPage.loggedAs.getText(), "Вошли как " + user.getLogin());
-        Assert.assertEquals(headerPage.userPage.getText(), user.getLogin());
-        //3. В заголовке страницы отображаются элементы: "Домашняя страница", "Моя страница", "Проекты", "Администрирование", "Помощь", "Моя учётная запись", "Выйти"
-        Assert.assertTrue(headerPage.homepage.isDisplayed());
-        Assert.assertTrue(headerPage.myPage.isDisplayed());
-        Assert.assertTrue(headerPage.projects.isDisplayed());
-        Assert.assertTrue(headerPage.help.isDisplayed());
-        Assert.assertTrue(headerPage.myAccount.isDisplayed());
-        Assert.assertTrue(headerPage.exit.isDisplayed());
-        //4. В заголовке страницы не отображаются элементы "Войти", "Регистрация"
-        Assert.assertFalse(BrowserUtils.isElementPresent(headerPage.login));
-        Assert.assertFalse(BrowserUtils.isElementPresent(headerPage.accountRegister));
-        Assert.assertFalse(BrowserUtils.isElementPresent(headerPage.administration));
+        AllureAssert.assertEquals(
+                headerPage.loggedAs.getText(),
+                "Вошли как " + user.getLogin(),
+                "Отображается \"Вошли как " + user.getLogin() + "\"");
+        //3. В заголовке страницы отображаются элементы: "Домашняя страница", "Моя страница", "Проекты", "Помощь", "Моя учётная запись", "Выйти"
+        AllureAssert.assertTrue(headerPage.homepage.isDisplayed(), "Отображается элемент Домашняя страница");
+        AllureAssert.assertTrue(headerPage.myPage.isDisplayed(), "Отображается элемент Моя страница");
+        AllureAssert.assertTrue(headerPage.projects.isDisplayed(), "Отображается элемент Проекты");
+        AllureAssert.assertTrue(headerPage.help.isDisplayed(), "Отображается элемент Помощь");
+        AllureAssert.assertTrue(headerPage.myAccount.isDisplayed(), "Отображается элемент Моя учётная запись");
+        AllureAssert.assertTrue(headerPage.exit.isDisplayed(), "Отображается элемент Выйти");
+        //4. В заголовке страницы не отображаются элементы "Войти", "Регистрация", "Администрирование"
+        AllureAssert.assertFalse(BrowserUtils.isElementPresent(headerPage.login), "Отображается элемент Войти");
+        AllureAssert.assertFalse(BrowserUtils.isElementPresent(headerPage.accountRegister), "Отображается элемент Регистрация");
+        AllureAssert.assertFalse(BrowserUtils.isElementPresent(headerPage.administration), "Отображается элемент Администрирование");
         //5. Отображается элемент "Поиск"
-        Assert.assertTrue(headerPage.searchField.isDisplayed());
-        Assert.assertTrue(headerPage.searchLink.isDisplayed());
+        AllureAssert.assertTrue(headerPage.searchField.isDisplayed(), "Отображается поле поиска");
+        AllureAssert.assertTrue(headerPage.searchLink.isDisplayed(), "Отображается элемент Поиск:");
     }
 
 }

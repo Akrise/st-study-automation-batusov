e9efd535565c4cd9ea2787c8c0f3f6e16feea643
Akrise <49941855+Akrise@users.noreply.github.com>
2021-12-25 16:32:46 +0300

    Добавлена работа с системными переменными

--- src/main/java/at/study/redmine/property/Property.java
+++ src/main/java/at/study/redmine/property/Property.java
@@ -12,24 +12,27 @@ public class Property {
     private static boolean isInitialized = false;
 
     @SneakyThrows
-    private static void init(){
+    private static void init() {
         properties.load(new FileInputStream("src/test/java/resources/" + propertiesName));
         isInitialized = true;
     }
 
-    public static String getStringProperty(String key){
-        if (!isInitialized){
+    public static String getStringProperty(String key) {
+        if (!isInitialized) {
             init();
+        }
+        if (System.getProperty(key) != null) {
+            return System.getProperty(key);
         }
         return properties.getProperty(key);
     }
 
-    public static Integer getIntegerProperty(String key){
+    public static Integer getIntegerProperty(String key) {
         return Integer.parseInt(getStringProperty(key));
     }
 
-    public static Boolean getBooleanProperty(String key){
-        return  Boolean.parseBoolean(getStringProperty(key));
+    public static Boolean getBooleanProperty(String key) {
+        return Boolean.parseBoolean(getStringProperty(key));
     }
 }
 
